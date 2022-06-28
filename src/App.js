1a619b2e31a91f5538bca1c175f648a790851b5f
sundarleo11 <sundarrock1999@gmail.com>
2022-06-28 22:05:09 +0530

    added table with color

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
+const conatiner = { color: "yellow", border: "1px solid red", paddingTop: "5px", paddingBottom: "5px", width: "300px" }
+
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h2>Hello world</h2>
+        <div style={conatiner}>Name List</div>
+        <div style={conatiner}>Ram</div>
+        <div style={conatiner}>Kumar</div>
+        <div style={conatiner}>sam</div>
       </header>
     </div>
   );
