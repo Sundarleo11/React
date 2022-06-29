9225062010043a6cd1381b1b2df653c1c0791505
sundarleo11 <sundarrock1999@gmail.com>
2022-06-29 20:50:12 +0530

    prope input

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 import H3 from './comp/name';
+import Input from './input/input';
 
 const conatiner = { color: "yellow", border: "1px solid red", paddingTop: "5px", paddingBottom: "5px", width: "300px" }
 
@@ -12,9 +13,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div style={conatiner}>Name List</div>
-
         <H3 firstname='saravan' lastname='kumar'></H3>
-        <H3 firstname='sathi' lastname='veli'></H3>
+        <Input placeholder="enter the value" type="text"></Input>
+
+
 
 
 
---
+++ src/input/input.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function input({ placeholder, type }) {
+
+    return <input placeholder={placeholder} type={type}></input>
+}
+
+export default input;
